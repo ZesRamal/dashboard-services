a48dca4186337d5d6b116c7646358ded46d5e1c5
ZesRamal <cesarfranramos@gmail.com>
2024-09-22 00:46:10 -0700

    express configured

--- dashboard-template/src/App.jsx
+++ dashboard-template/src/App.jsx
@@ -1,8 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home, Register, Registry, Login } from "./pages";
 import { Navbar } from './layout';
+import { useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const fetchAPI = async () => {
+    const response = await axios.get("http://localhost:3000/")
+    console.log(response.data);
+  }
+
+  useEffect(() => {
+    fetchAPI()
+  }, [])
+
   return (
     <div style={{ display: "flex", backgroundColor: "rgb(217, 230, 247)" }}>
       <Router>
--- server/server.js
+++ server/server.js
@@ -1,15 +1,12 @@
-import { createClient } from 'redis';
-
-export default async function startConnection(){
-    const client = createClient({
-        password: import.meta.env.VITE_REDIS_PASSWORD,
-        socket: {
-            host: 'redis-13347.c60.us-west-1-2.ec2.redns.redis-cloud.com',
-            port: 13347
-        }
-    });
-    
-    client.on('error', (err) => console.log('Redis Client Error', err));
-    
-    await client.connect();
+const express = require('express')
+const app = express()
+const port = 3000
+const cors = require('cors')
+const corsOptions = {
+    origin: "http://localhost:5173"
 }
+
+app.use(cors(corsOptions));
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
